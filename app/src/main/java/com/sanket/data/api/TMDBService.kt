7204f140be4829c76cd9eb34bb7e3147a9b6f0b9
ishanishah21 <ishanishah2110@gmail.com>
2022-01-25 01:18:41 +0530

    - Updated the Service class name

--- app/src/main/java/com/sanket/data/api/TMDBService.kt
+++ app/src/main/java/com/sanket/data/api/TMDBService.kt
@@ -7,7 +7,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface TMDBService {
+interface TMDBServices {
     @GET("/movie/popular")
     suspend fun getPopularMovies(@Query("api_key") apiKey: String): Response<MovieList>
 
